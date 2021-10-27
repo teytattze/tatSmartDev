da33a78428dcc1e2f656501d97b494cbd21cf818
tattzetey <tattzetey@gmail.com>
2021-10-27 23:52:50 +0100

    fix relative path

--- src/modules/personal/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './components/personal-home';
-export * from './components/personal-about';
-export * from './components/personal-resume';
-export * from './components/personal-portfolio';
-export * from './components/personal-contact';
