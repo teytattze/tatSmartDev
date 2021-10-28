64ea8acd71d87acac07d2a26632cbbe874e5ea21
tattzetey <tattzetey@gmail.com>
2021-10-28 17:26:17 +0100

    set default theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,7 +24,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
           },
         ]}
       />
-      <ThemeProvider>
+      <ThemeProvider defaultTheme="dark">
         <Layout route={router.route}>
           <Component {...pageProps} />
         </Layout>
